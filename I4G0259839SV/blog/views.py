e7a6760bfc580ac010d0371f89f372792ac21456
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-06-26 00:13:33 +0100

    CRUD

---
+++ I4G0259839SV/blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.detail import DetailView
+from .models import Post
+
+from django.urls import reverse_lazy
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
